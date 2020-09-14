945cc193e44dfc494f8096465a5a71017c09cd93
tim humphrey <tlhumphrey2@gmail.com>
2020-09-14 15:52:38 -0400

    fixed root execution.
    Signed-off-by: tim humphrey <tlhumphrey2@gmail.com>

--- main.tf
+++ main.tf
@@ -169,6 +169,7 @@ resource "azurerm_virtual_machine" "cluster-node" {
   provisioner "remote-exec" {
     inline = [
       "#Setup logging and having everything goto /var/log/user-data.log",
+      "sudo su -",
       "exec 3>&1 4>&2",
       "trap 'exec 2>&4 1>&3' 0 1 2 3",
       "exec 1>/var/log/user-data.log 2>&1",
