23101d8601b6b5aa0796fafa765c0b0ae70d8775
tim humphrey <tlhumphrey2@gmail.com>
2020-09-15 12:09:23 -0400

    Fixed 'sudo -S ls -l' so password is not needed for 'sudo'.
    Signed-off-by: tim humphrey <tlhumphrey2@gmail.com>

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ resource "azurerm_virtual_machine" "cluster-node" {
   provisioner "remote-exec" {
     inline = [
       "#Setup logging and having everything goto /home/adminuser/user-data.log",
-      "echo ${var.admin_password} | sudo -S ls -l;sudo su -",
+      "echo ${var.admin_password} | sudo -S ls -l",
       "exec 3>&1 4>&2",
       "trap 'exec 2>&4 1>&3' 0 1 2 3",
       "exec 1>/home/adminuser/user-data.log 2>&1",
